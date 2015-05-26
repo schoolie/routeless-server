54286c1cc3875eb7340716c9a3f6d7d75cc7faf2
schoolie <brian.p.schoolcraft@gmail.com>
2015-05-26 14:12:54 -0400

    standardizing api url

--- routeless/__init__.py
+++ routeless/__init__.py
@@ -39,8 +39,8 @@ def create_app(config_name):
     # from .api_1_0 import api as api_1_0_blueprint
     # app.register_blueprint(api_1_0_blueprint, url_prefix='/api/v1.0')
     
-    QuotesView.register(app, route_base='/api/')
-    UsersView.register(app)
+    # QuotesView.register(app, route_base='/api_1_0/')
+    UsersView.register(app, route_base='/api_1_0/users')
     
     print app.url_map
     
