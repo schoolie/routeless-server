7497f160833d1e90c5e9ceede1b68fc3f7c501c7
schoolie <brian.p.schoolcraft@gmail.com>
2015-05-27 16:53:54 -0400

    trying to get post from angular to work

--- routeless/__init__.py
+++ routeless/__init__.py
@@ -1,5 +1,5 @@
 # Embedded file name: C:\Development\routeless-server\routeless\__init__.py
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask.ext.mail import Mail
 from flask.ext.moment import Moment
 from flask.ext.login import LoginManager
@@ -28,7 +28,8 @@ def create_app(config_name):
     db.init_app(app)
     login_manager.init_app(app)
     api_manager.init_app(app)
-    cors.init_app(app, resources={"/api_1_0/*": {"origins": "http://localhost:8383"}})
+    # cors.init_app(app, resources={"/api_1_0/*": {"origins": "http://localhost:8383"}})
+    cors.init_app(app, resources={"/*": {"origins": "http://localhost:8383"}})
     
     if not app.debug and not app.testing and not app.config['SSL_DISABLE']:
         from flask.ext.sslify import SSLify
@@ -58,6 +59,11 @@ def create_app(config_name):
                            )
         app.register_blueprint(event_api)
     
+    @app.route('/test', methods = ['POST'])
+    def test():
+        print request.data
+        # pdb.set_trace()
+        return 'done'
     
     print app.url_map
     
