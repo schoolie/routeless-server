295f363934e8e9d33fbe549dd63dd99af6ab8355
schoolie <brian.p.schoolcraft@gmail.com>
2015-09-09 16:01:36 -0400

    Cleaned up models

--- config.py
+++ config.py
@@ -12,13 +12,9 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
-    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
-    FLASKY_POSTS_PER_PAGE = 20
-    FLASKY_FOLLOWERS_PER_PAGE = 50
-    FLASKY_COMMENTS_PER_PAGE = 30
-    FLASKY_SLOW_DB_QUERY_TIME = 0.5
+    ROUTELESS_MAIL_SUBJECT_PREFIX = '[Routeless]'
+    ROUTELESS_MAIL_SENDER = 'Routeless Admin <admin@routeless.co>'
+    ROUTELESS_ADMIN = os.environ.get('FLASKY_ADMIN')
 
     JWT_EXPIRATION_DELTA = 3600
     
@@ -27,20 +23,6 @@ class Config:
     @staticmethod
     def init_app(app):
         pass
-
-
-class DevelopmentConfig(Config):
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
-
-class DevServerConfig(Config):
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
-
-class TestingConfig(Config):
-    TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
-    WTF_CSRF_ENABLED = False
 
 
 class ProductionConfig(Config):
@@ -62,22 +44,6 @@ class ProductionConfig(Config):
         app.logger.addHandler(mail_handler)
         return
 
-
-class HerokuConfig(ProductionConfig):
-    SSL_DISABLE = bool(os.environ.get('SSL_DISABLE'))
-
-    @classmethod
-    def init_app(cls, app):
-        ProductionConfig.init_app(app)
-        from werkzeug.contrib.fixers import ProxyFix
-        app.wsgi_app = ProxyFix(app.wsgi_app)
-        import logging
-        from logging import StreamHandler
-        file_handler = StreamHandler()
-        file_handler.setLevel(logging.WARNING)
-        app.logger.addHandler(file_handler)
-
-
 class UnixConfig(ProductionConfig):
 
     @classmethod
@@ -89,11 +55,22 @@ class UnixConfig(ProductionConfig):
         syslog_handler.setLevel(logging.WARNING)
         app.logger.addHandler(syslog_handler)
 
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+class DevServerConfig(UnixConfig):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+    WTF_CSRF_ENABLED = False
 
 config = {'development': DevelopmentConfig,
  'dev_server': DevServerConfig,
  'testing': TestingConfig,
  'production': ProductionConfig,
- 'heroku': HerokuConfig,
  'unix': UnixConfig,
  'default': DevelopmentConfig}
